c60e642b43a83ea383922fcc91e5368246ead2dd
oraziogallo <orazio.gallo@gmail.com>
2020-04-16 07:42:37 -0700

    Improved install.sh. - Fixed bug that would over-write `~/.bash_aliases' - Improved handling of potential permission errors

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 cp fprm.sh /usr/local/bin/
+if [ $? -ne 0 ]; then
+  echo "Copy failed. Run with sudo?" >&2
+  exit 1
+fi
 
-if [ -f '~/.bash_aliases' ]; then
+if [ -f ~/.bash_aliases ]; then
     echo "alias rm='/usr/local/bin/fprm.sh'" >> ~/.bash_aliases
 else
     echo "alias rm='/usr/local/bin/fprm.sh'" > ~/.bash_aliases
 fi
+
+exit 0
