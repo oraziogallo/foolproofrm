ba8c93a887af0c5408de1b0dae89ba106102c80a
oraziogallo <orazio.gallo@gmail.com>
2020-05-16 18:25:57 -0700

    Version 0.2: - When called with -D the script correclty passes the remaining flags to /bin/rm - Improved readme file including references to alternatives - Added MIT license - Minor bug fix for exit codes

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Fool-proof rm
-Small tool to prevent involuntarily `rm`'ing important files.
-Fool-proof rm works like rm but, instead of permanently deleting files and folders, it time-stamps and moves them to its trash folder. 
+Have you ever `rm`'ed  something by mistake? It happens to the best of us, including the creators of Toy Story 2 (Try to Google "Toy Story 2 rm").  
+`Fool-proof rm` is a small tool to prevent exactly that.
+You use it and call it exactly like `rm` but, instead of permanently deleting files and folders, it time-stamps and moves them to its own trash folder. [Similar tools](#Alternative-tools) either define a list of protected folders, or implement a trash managment that requires specific commands.
 
 ## Usage
-After installation, the `rm` command will move the files or folders to the trash folder and prepend the time of deletion to the filename. The flags are the same as the native `rm` (see currently supported flags below).  
+After installation, calling `rm` will move files or folders to the trash folder and prepend the time of deletion to the filename. The flags are the same as the native `rm` (see currently supported flags below).
 To permanently delete files (traditional, dangerous behavior) use the flag `-D`.
 The standard `rm` command can still be run using the fully qualified path (generally `/bin/rm`).
 
 ### Supported flags
 | Flag | Supported/New | Comments|
 | :---:|  :---:    |  :---:  |
-| -D | New | Behave as traditional `rm -rf` |
+| -D | New | Behave as traditional `rm`<sup>1</sup>|
 | -E | New | Empty trash |
 | -r | Yes | --- |
 | -f | Partial | Don't prompt for read-only files |
@@ -19,10 +20,23 @@ The standard `rm` command can still be run using the fully qualified path (gener
 | -i | Not yet | --- |
 | --version | Not yet | --- |
 
+<sup>1</sup> The rest of the flags are passed to `rm` directly. For instance, `rm -Dfv testfile` maps to `/bin/rm -fv testfile`.
+
 ## Installation
-The installation simply creates an alias to the script that "shadows" `rm`.
+Clone this repository and run:
 ```bash
 bash install.sh
 source ~/.bashrc
 ```
-Similarly, to uninstall simply remove the `rm` alias in `~/.bash_aliases`. 
+The installation simply creates an alias to the script that "shadows" `rm` making the use of `fool-proof rm` transparent to the user.
+No other change is made to the system.
+Of course, you can also create a different alias--the script is totally self-contained.
+Similarly, simply remove the `rm` alias in `~/.bash_aliases` to uninstall. 
+
+## Alternative tools
+* [safe-rm](http://manpages.ubuntu.com/manpages/bionic/en/man1/safe-rm.1.html)  
+Prevents removal of a predefined list of folders.
+* [rm-protection](https://github.com/alanzchen/rm-protection)  
+Similar to `safe-rm` but asks for confirmation instead of preventing deletion.
+* [trash-cli](https://github.com/andreafrancia/trash-cli)  
+Implements a trash system, though it has its own commands that differ from `rm`.
--- fprm.sh
+++ fprm.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+
+# Fool-proof rm
+# Copyright (C) 2020  Orazio Gallo
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the Software
+# is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+# IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 TRASH_DIR="/home/"$USER"/.trash_foolproofrm"
-VERSION="0.1"
+VERSION="0.2"
 
 usage() {
   cat << EOF >&2
@@ -16,7 +35,6 @@ Remove (safely move to trash) the FILE(s).
 
 foolproofrm v$VERSION
 EOF
-  exit 1
 }
 
 clear_trash() {
@@ -48,15 +66,16 @@ then
 fi
 
 # Parse the inputs
+flags="-"
 while getopts fvrhTDE o
 do
   case $o in
-    f) force=1;;
-    v) verbose=1;;
-    r) recursive=1;;
+    f) force=1; flags+=$o;;
+    v) verbose=1; flags+=$o;;
+    r) recursive=1; flags+=$o;;
     D) remove=1;;
     E) clear_trash;;
-    h|*) usage;;
+    h|*) usage; exit 0;
   esac
 done
 shift "$((OPTIND - 1))"
@@ -65,12 +84,13 @@ file="$@"
 if [[ $# -eq 0 ]]; then
   echo "Missing operand"
   usage
+  exit 1
 fi
 
 # If the feared -D argument was passed, just call rm
 if [[ $remove ]]; then
-  /bin/rm -rf "$@"
-  exit 0
+  eval "/bin/rm $flags $@"
+  exit $?
 fi
 
 for file; do
@@ -121,52 +141,3 @@ for file; do
 done
 
 exit 0
-
-# Getops from the getops manual:
-# https://pubs.opengroup.org/onlinepubs/9699919799/utilities/getopts.html
-# aflag=
-# bflag=
-# while getopts ab: name
-# do
-#     case $name in
-#     a)    aflag=1;;
-#     b)    bflag=1
-#           bval="$OPTARG";;
-#     ?)   printf "Usage: %s: [-a] [-b value] args\n" $0
-#           exit 2;;
-#     esac
-# done
-# if [ ! -z "$aflag" ]; then
-#     printf "Option -a specified\n"
-# fi
-# if [ ! -z "$bflag" ]; then
-#     printf 'Option -b "%s" specified\n' "$bval"
-# fi
-# shift $(($OPTIND - 1))
-# printf "Remaining arguments are: %s\n" "$*"
-
-
-# Getops from Stackoverflow post:
-# https://unix.stackexchange.com/questions/321126/dash-arguments-to-shell-scripts
-# #! /bin/sh -
-# PROGNAME=$0
-# usage() {
-#   cat << EOF >&2
-# Usage: $PROGNAME [-v] [-d <dir>] [-f <file>]
-# -f <file>: ...
-#  -d <dir>: ...
-#        -v: ...
-# EOF
-#   exit 1
-# }
-# dir=default_dir file=default_file verbose_level=0
-# while getopts d:f:v o; do
-#   case $o in
-#     (f) file=$OPTARG;;
-#     (d) dir=$OPTARG;;
-#     (v) verbose_level=$((verbose_level + 1));;
-#     (*) usage
-#   esac
-# done
-# shift "$((OPTIND - 1))"
-# echo Remaining arguments: "$@"
